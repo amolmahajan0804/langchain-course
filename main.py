4c1fa045c14b172e59465b117c3ede8821f420c2
amol mahajan <48409675+amolmahajan0804@users.noreply.github.com>
2026-05-07 13:10:45 +0530

    using lanchain integrated tavily tool

--- main.py
+++ main.py
@@ -1,28 +1,33 @@
 from dotenv import load_dotenv
+load_dotenv()
+
+
 from langchain.agents import create_agent
 from langchain.messages import HumanMessage
-from langchain.tools import tool
 from langchain_ollama.chat_models import ChatOllama
-from tavily import TavilyClient
+# from langchain.tools import tool
+# from tavily import TavilyClient
+from langchain_tavily import TavilySearch
 
-load_dotenv()
-tavily = TavilyClient()
+
+# tavily = TavilyClient()
 
 
-@tool
-def tavily_search(query: str) -> str:
-    """
-    Tool that searches over internet.
-    arg:
-        query:- The search term given by user.
-    returns:
-        The serch result.
-    """
-    return tavily.search(query=query)
+# @tool
+# def tavily_search(query: str) -> str:
+#     """
+#     Tool that searches over internet.
+#     arg:
+#         query:- The search term given by user.
+#     returns:
+#         The serch result.
+#     """
+#     return tavily.search(query=query)
 
 
 llmmodel = ChatOllama(model='llama3.2:latest', temperature=0.2)
-customtools = [tavily_search]
+# customtools = [tavily_search]
+customtools = [TavilySearch()]
 
 agent = create_agent(model=llmmodel, tools=customtools)
 response = agent.invoke({'messages':HumanMessage('what is current weather in punawale, pune?')} )
